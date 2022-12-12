e0c62be1997901540e959b07b71459ffb454f147
François C <francois.caremoli@wanadoo.fr>
2022-12-12 15:43:13 +0100

    Exercice instanciations

--- src/exercices/ExerciceInstanciations.java
+++ src/exercices/ExerciceInstanciations.java
@@ -4,11 +4,14 @@ package exercices;
 public class ExerciceInstanciations {
 	public static void main(String[] args) {
 		// Définir une variable de type Object
+		Object monObjet;
 		
-		//Définition et instanciation d'un nouvel objet dans une nouvelle variable
+		//Définition et instanciation(création d'une nouvelle instance)
+		//d'un nouvel objet dans une nouvelle variable
+		Object monDeuxiemeObjet = new Object();
 		
 		//Affectation de la valeur null à cet objet
-		
+		monDeuxiemeObjet = null;
 		
 
 	}
