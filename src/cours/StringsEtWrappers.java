f05836d33d87c546b8dbf8a0ff218d9a852bd194
François C <francois.caremoli@wanadoo.fr>
2022-12-13 15:48:52 +0100

    Ajout des exercices sur les Wrappers

--- src/cours/StringsEtWrappers.java
+++ src/cours/StringsEtWrappers.java
@@ -13,22 +13,30 @@ public class StringsEtWrappers{
 		maChaineDeCaractèresNonNull += " les gens";
 		
 		// Autoboxing
+		Integer monEntier = 4;
         
         // Unboxing
+		Integer monAutreEntier = Integer.valueOf(4);
+		monAutreEntier++;
         
         // Conversion d'une String en classe Float
+		Float f = Float.valueOf("3.4");
         
         // Conversion du float en String
+		f.toString();
 		
-
         // Saisir au clavier et affecter la saisie a une variable
 		Scanner scannerClavier = new Scanner(System.in);
 		System.out.println("Veuillez entrer une chaîne de caractères");
 		String venantDuClavier = scannerClavier.nextLine();
-		System.out.println("La chaine de caractères vaut  " + venantDuClavier);
+		System.out.println("La chaine de caractères vaut : " + venantDuClavier);
         
         // Recuperer un double
+		System.out.println("Veuillez entrer un float ");
+		String doubleAsString = scannerClavier.nextLine();
+		System.out.println("La chaine de caractères vaut : " + Double.valueOf(doubleAsString));
 		
 		
+		scannerClavier.close();
 	}
 }
--- src/exercices/ExerciceWrappers.java
+++ src/exercices/ExerciceWrappers.java
@@ -5,14 +5,13 @@ public class ExerciceWrappers {
 	public static void main(String[] args) {
 		
 		//Boxing d'un boolean (true) en Boolean
+		Boolean bool = true;
 		
 		//Création d'un int à partir d'une chaîne de caractères
-		
+		int monInt = Integer.valueOf("43");
 		
 		//Passage de ce int en Integer 
-		
-		
-		
+		Integer monIntAsInteger = monInt  + 2;
 		
 
 	}
