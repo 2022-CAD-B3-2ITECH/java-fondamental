5bace0a92d00d9b75282f65d68d2f5d20be8b16d
François C <francois.caremoli@wanadoo.fr>
2022-12-14 09:45:02 +0100

    Ajout du cours sur les conditions

--- src/cours/Conditions.java
+++ src/cours/Conditions.java
@@ -1,22 +1,76 @@
 package cours;
 
+import simulateur.bateau.Bateau;
+
 // Classe principale
 public class Conditions {
 	public static void main(String[] args) {
-		short forceElephant = 10;
+		short forceElephant = 1;
 		short forceRhinoceros = 12;
 
 		// if then else
+		if (forceElephant > forceRhinoceros) {
+			System.out.println("L'éléphant est le plus fort");
+		} else {
+			if (forceElephant == forceRhinoceros) {
+				System.out.println("Les deux animaux sont de force égale");
+			} else {
+				System.out.println("Le rhinocéros est le plus fort");
+			}
+		}
 
 		// switch
+		System.out.println("\r\nSwitch");
+		switch (forceElephant) {
+		case 1:
+			System.out.println("L'éléphant a une force de 1");
+			System.out.println("Il n'est pas très fort");
+			break;
+		case 2:
+			System.out.println("L'éléphant a une force de 2");
+			System.out.println("Il n'est pas vraiment fort");
+			break;
+		default:
+			System.out.println("Je ne sais pas quelle force a l'éléphant");
+		}
 
-		//switch case avec blocs (plus besoin de break)
+		// switch case avec blocs (plus besoin de break)
+		System.out.println("\r\nSwitch avec blocs");
+		switch (forceElephant) {
+		case 1 -> {
+			System.out.println("L'éléphant a une force de 1");
+			System.out.println("Il n'est pas très fort");
+		}
+		default -> {
+			System.out.println("Je ne sais pas quelle force a l'éléphant");
+		}
+		}
 
-		//Switch case dont le résultat est stocké dans une variable avec yield
+		System.out.println("\r\nSwitch avec yield");
+		// Switch case dont le résultat est stocké dans une variable avec yield
+		Bateau bateau1 = null;
+		Bateau bateau = switch (forceElephant) {
+		case 1 -> {
+			yield new Bateau("l'éléphant est pas très fort.");
+		}
+		case 2 -> {
+			yield new Bateau("l'éléphant est ltrès fort ");
+		}
+		default -> {
+			yield new Bateau("Qu'est ce que c'est que ces noms de bateaux ?");
+		}
+		};
+		bateau.getNom();
+		
 
+		System.out.println("\r\nConditions ternaires");
 		// Condition ternaire
-		
+		String messageAAfficher = forceElephant > forceRhinoceros ? "elephant plus fort" : "rhino plus fort";
+
 		// Condition ternaire composée
-		
+		String messageAAfficher2 = forceElephant > forceRhinoceros 
+					? "elephant plus fort" : forceElephant == forceRhinoceros 
+						? "forces égales": "rhino plus fort";
+
 	}
 }
